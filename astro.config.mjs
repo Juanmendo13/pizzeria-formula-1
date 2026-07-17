59b47e76fd59fd0e8cceb582b4ac26b60fbf9e14
juanmendo <jmendozadame@gmail.com>
2026-07-17 02:53:37 +0200

    feat: integrado menu de pizzas y solucionado error de sharp para vercel, ademas tenemos hasta la pizza numero 10 en el menu ya

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,8 @@
 import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind'; // O el adaptador que tengas
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
-  // ... tus integraciones si las tienes
-  integrations: [tailwind()],
-
   vite: {
-    server: {
-      allowedHosts: [
-        '.loca.lt',                  // Esto permite CUALQUIER subdominio de localtunnel
-        'dull-onions-own.loca.lt'    // Por si acaso, tu dominio actual exacto
-      ]
-    }
-  }
+    plugins: [tailwindcss()],
+  },
 });
